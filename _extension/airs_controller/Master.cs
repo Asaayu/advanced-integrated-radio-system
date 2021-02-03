8be6fe06b21dbdaae560ceb977c2d1ad24fc91ef
Ethan <wowsilverhoof2@gmail.com>
2021-02-03 20:35:16 +1300

    Updates to ts sdk stuff

--- _extension/airs_controller/Master.cs
+++ _extension/airs_controller/Master.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using TeamSpeak.Sdk;
+using TeamSpeak.Sdk.Client;
 
 namespace airs_controller
 {
@@ -44,8 +46,7 @@ namespace airs_controller
             outputSize--;
 
             Log.Setup();
-
-            Log.Info("DLL loaded by game...");            
+            Log.Info("Setup successful...");
 
             output.Append("AIRS VOIP - " + App.version);
         }
@@ -66,23 +67,7 @@ namespace airs_controller
         }
     }
 
-    public class Internal
-    {
-        internal static double[] pos_asl = new double[3];
-        internal static double[] vector_dir = new double[3];
-        internal static double[] vector_up = new double[3];
-
-        internal static bool Update(double[] pos, double[] dir, double[] up)
-        {
-            // Update variables
-            pos_asl = pos;
-            vector_dir = dir;
-            vector_up = up;
-            return true;
-        }
-    };
-
-    public class Functions
+    public static class Functions
     {
         internal static string Main(string input)
         {
@@ -95,27 +80,159 @@ namespace airs_controller
 
             switch (parameters[0])
             {
-                // UPDATE: Update the internal position, direction and up vectors in 3D space.
-                case "update":
+                // APPLICATION_START: Is run when the Arma 3 application starts
+                case "application_start":
                     try
                     {
-                        double[] pos = new double[3] { double.Parse(parameters[1]), double.Parse(parameters[2]), double.Parse(parameters[3]) };
-                        double[] dir = new double[3] { double.Parse(parameters[4]), double.Parse(parameters[5]), double.Parse(parameters[6]) };
-                        double[] up = new double[3] { double.Parse(parameters[7]), double.Parse(parameters[8]), double.Parse(parameters[9]) };
-                        Internal.Update(pos, dir, up);
+                        Log.Info("'APPLICATION_START' Starting...");
+
+
+                        Log.Info("'APPLICATION_START' Ending...");
                     }
                     catch (Exception e)
                     {
-                        Log.Info("Error updating position, direction, and up vectors...");
+                        Log.Info("'APPLICATION_START' Error:");
                         Log.Error(e.ToString());
                     }
-                    return "";
+                    return "true";
+
+                // PLAYER_POS: Set the players position, velocity, and orentation
+                case "player_pos":
+                    try
+                    {
+                        Log.Info("'PLAYER_POS' Starting...");
+
+                        List<float> inputs = new List<float>();
+                        foreach (string i in parameters)
+                        {
+                            inputs.Add(float.Parse(i));
+                        };
+
+                        Vector pos = new Vector(inputs[0], inputs[1], inputs[2]);
+                        Vector forward = new Vector(inputs[3], inputs[4], inputs[5]);
+                        Vector up = new Vector(inputs[6], inputs[7], inputs[8]);
+
+                        TS_SDK.connection.Set3DListenerAttributes(pos, forward, up);
+                        Log.Info("'PLAYER_POS' Ending...");
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Info("'PLAYER_POS' Error:");
+                        Log.Error(e.ToString());
+                    }
+                    return "true";
 
                 // INFO: Show version information
                 case "info":
                     return App.version_info;
             }
             return "";
+        }
+    };
+
+    public class TS_SDK
+    {
+        static byte[] WaveHeader = new byte[]
+        {
+            0x52, 0x49, 0x46, 0x46, 0x00, 0x00, 0x00, 0x00, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20,
+            0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02, 0x00, 0x80, 0xBB, 0x00, 0x00, 0x00, 0xB8, 0x0B, 0x00,
+            0x10, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61, 0x00, 0x00, 0x00, 0x00
+        };
+
+        internal static Connection connection;
+        static bool abort = false;
+        const string identityfile = "identity.txt";
+        static FileStream wavefile;
+
+        static void Main()
+        {
+            Initialize();
+
+            connection.OpenPlayback();
+            Console.WriteLine($"Playback: {connection.PlaybackDevice}");
+            connection.OpenCapture();
+            Console.WriteLine($"Capture: {connection.CaptureDevice}");
+
+            string identity = ReadIdentity() ?? Library.CreateIdentity();
+            Task starting = connection.Start(identity, "localhost", 9986, "client", serverPassword: "secret");
+
+            Console.WriteLine("Client lib initialized and running");
+            Console.WriteLine($"Client lib version: {Library.Version}({Library.VersionNumber})");
+        }
+
+        private static void Initialize()
+        {
+            LibraryParameters parameters = new LibraryParameters("ts3_sdk_3.0.4/bin/");
+            parameters.UsedLogTypes = LogTypes.File | LogTypes.Console | LogTypes.Userlogging;
+            Library.Initialize(parameters);
+
+            connection = Library.SpawnNewConnection();
+            connection.StatusChanged += Connection_StatusChanged;
+            connection.TalkStatusChanged += Connection_TalkStatusChanged;
+            connection.ServerError += Connection_ServerError;
+            //connection.EditMixedPlaybackVoiceData += Connection_EditMixedPlaybackVoiceData;
+            //connection.Custom3dRolloffCalculationClient += Connection_Custom3dRolloffCalculationClient;
+        }
+
+        private static void Connection_StatusChanged(Connection connection, ConnectStatus newStatus, Error error)
+        {
+            Log.Info("Status Changed:");
+            Log.Info(newStatus.ToString());
+        }
+
+        private static void Connection_ServerError(Connection connection, Error error, string returnCode, string extraMessage)
+        {
+            switch (error)
+            {
+                case Error.ConnectionLost:
+                    Log.Error("Client lost connection to the server!");
+                    break;
+                
+                case Error.SoundCouldNotOpenCaptureDevice:
+                    Log.Error("Could not open capture device!");
+                    break;
+                
+                case Error.SoundCouldNotOpenPlaybackDevice:
+                    Log.Error("Could not open playback device!");
+                    break;
+
+                default:
+                    Log.Error("And error has occured:");
+                    Log.Error(extraMessage);
+                    break;
+            }
+        }
+
+        private static void Connection_TalkStatusChanged(Client client, TalkStatus status, bool isReceivedWhisper)
+        {
+            if (status == TalkStatus.Talking)
+            {
+                if (client.ID == connection.ID)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private static string ReadIdentity()
+        {
+            try
+            {
+                return File.ReadAllText(identityfile);
+            }
+            catch
+            {
+                Console.WriteLine($"Could not read file '{identityfile}'.");
+                return null;
+            }
         }
     };
 
