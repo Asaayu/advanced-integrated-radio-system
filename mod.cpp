ff37f6d4f404f1fd53fe7576630166c48234e9fc
Ethan Johnson <37124195+Asaayu@users.noreply.github.com>
2025-01-01 23:20:44 +1300

    feat: setup hemtt

---
+++ mod.cpp
@@ -0,0 +1,15 @@
+name = "Advanced Integrated Radio System";
+
+actionName = "Workshop";
+action = "https://steamcommunity.com/id/asaayu/myworkshopfiles/";
+author= "Asaayu";
+
+picture = "\z\airs\addons\main_f_airs\data\picture_ca.paa";
+logo = "\z\airs\addons\main_f_airs\data\logo_ca.paa";
+logoOver = "\z\airs\addons\main_f_airs\data\logo_over_co.paa";
+logoSmall = "\z\airs\addons\main_f_airs\data\logo_small_ca.paa";
+
+tooltip = "Advanced Integrated Radio System";
+tooltipOwned = "Advanced Integrated Radio System - Owned";
+overview = "Communicate with others using an integrated advanced radio system.";
+description = "Communicate with others using an integrated advanced radio system.";
